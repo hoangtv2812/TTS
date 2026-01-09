a98d30750c6d045cc5d5f43f731dd87059a78089
Truong Viet Hoang <hoangtv@Truongs-Mac-mini.local>
2026-01-09 16:02:25 +0700

    test 4

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+throw new Error('Frontend app crashed in production');
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
